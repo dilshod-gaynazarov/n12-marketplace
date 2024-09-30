443e858df1b422c869a748dee42b1139668d461b
dilshod-gaynazarov <dilshod7861@gmail.com>
2024-09-30 11:46:14 +0500

    utils updated

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -3,13 +3,13 @@ import { InternalServerErrorException } from "@nestjs/common"
 export const generateTokens = async (jwtService: any, payload: object) => {
     try {
         const [access_token, refresh_token] = await Promise.all([
-            await jwtService.sign(payload, {
-                secretKey: process.env.ACCESS_TOKEN_KEY,
-                expireIn: process.env.ACCESS_TOKEN_TIME
+            jwtService.sign(payload, {
+                secret: process.env.ACCESS_TOKEN_KEY,
+                expiresIn: process.env.ACCESS_TOKEN_TIME
             }),
-            await jwtService.sign(payload, {
-                secretKey: process.env.REFRESH_TOKEN_KEY,
-                expireIn: process.env.REFRESH_TOKEN_TIME
+            jwtService.sign(payload, {
+                secret: process.env.REFRESH_TOKEN_KEY,
+                expiresIn: process.env.REFRESH_TOKEN_TIME
             }),
         ]);
         return { access_token, refresh_token };
